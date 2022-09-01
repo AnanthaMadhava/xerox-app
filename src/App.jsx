30676d517540d185ab08a0b5d51ee57692ab1913
Anantha Madhava <anantha.madhava@hp.com>
2022-09-01 13:25:51 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import { Grommet, grommet } from 'grommet';
-import Content from './Content';
-import Header from './Header';
 
 const App = () => {
   return (
-    // <Grommet full theme={grommet} themeMode='light'>
-    //   <Header />
-    //   <Content />
-    // </Grommet>
     <div>
       <h1>Hello World</h1>
     </div>

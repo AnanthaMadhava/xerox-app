e9fdc28e9a33f6a984643dcc841092d1645c50b3
Anantha M <anantha.m@mphasis.com>
2022-10-30 10:53:49 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Grommet, grommet } from 'grommet';
 import Content from './Content';
 import Header from './Header';
+import axios from 'axios';
 
 const App = () => {
+
+  const [ userName, setUserName ] = useState('AnanthaMadhava');
+  const [ usersList, setUsersList ] = useState([]);
+
+  useEffect(() => {
+    if(userName) {
+        axios.get(`https://api.github.com/search/users?q=${userName}+in:user`)
+        .then(res => {
+          let result = res.data.items;
+          console.log(result);
+          setUsersList(result);
+        })
+        .catch(err => {
+          console.log(err.response);
+          setUsersList([]);
+        })
+    }
+  }, [ userName ]);
+
   return (
     <Grommet full theme={grommet} themeMode='light'>
       <Header />

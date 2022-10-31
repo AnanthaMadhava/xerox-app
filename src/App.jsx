20af0f707a794feffb7ee0ecd3e8d59ef15effc4
Anantha M <anantha.m@mphasis.com>
2022-10-31 21:09:32 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,30 @@
-import React, { useEffect } from 'react';
-import { Grommet, grommet } from 'grommet';
-import Content from './Content';
-import Header from './Header';
-import axios from 'axios';
-
-const App = () => {
-
-  // const [ userName, setUserName ] = useState('AnanthaMadhava');
-  // const [ usersList, setUsersList ] = useState([]);
-
-  useEffect(() => {
-    // if(userName) {
-        axios.get(`https://api.github.com/search/users?q=AnanthaMadhava+in:user`)
-        .then(res => {
-          let result = res.data.items;
-          console.log(result);
-          // setUsersList(result);
-        })
-        .catch(err => {
-          console.log(err.response);
-          // setUsersList([]);
-        })
-    // }
-  }, [ ]);
-
-  return (
-    <Grommet full theme={grommet} themeMode='light'>
-      <Header />
-      <Content />
-    </Grommet>
-  );
-};
-export default App;
-
 // import React from 'react';
-// import { Routes, Route } from 'react-router-dom';
-// import GitHubRepos from './component/js/container/GitHubRepos';
-// import GitHubUsers from './component/js/container/GitHubUsers';
+// import { Grommet, grommet } from 'grommet';
+// import Content from './Content';
+// import Header from './Header';
 
 // const App = () => {
 //   return (
-//     <Routes>
-//       <Route exact path='/' element={<GitHubUsers />} />
-//       <Route path='/repo/:userId' element={<GitHubRepos />} />
-//     </Routes>
+//     <Grommet full theme={grommet} themeMode='light'>
+//       <Header />
+//       <Content />
+//     </Grommet>
 //   );
 // };
-
 // export default App;
+
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import GitHubRepos from './component/js/container/GitHubRepos';
+import GitHubUsers from './component/js/container/GitHubUsers';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route exact path='/' element={<GitHubUsers />} />
+      <Route path='/repo/:userId' element={<GitHubRepos />} />
+    </Routes>
+  );
+};
+
+export default App;

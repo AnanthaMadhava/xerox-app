da42bfd73d54b40ec5b96533235ecf473cd81fa3
Anantha M <anantha.m@mphasis.com>
2022-11-08 22:45:02 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Grommet, grommet } from 'grommet';
 import Content from './Content';
 import Header from './Header';
@@ -6,13 +6,15 @@ import axios from 'axios';
 
 const App = () => {
 
+  const [ data, setData ] = useState([]);
+
   useEffect(() => {
     axios.get('https://jsonplaceholder.typicode.com/users')
       .then(res => {
-        console.log(res.data);
+        setData(res.data);
       })
       .catch(err => {
-        console.log(err.response);
+        setData([]);
       })
   }, [])
   return (
